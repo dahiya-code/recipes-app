afacaf5e18e4e33730ba9ba585a043758953fd71
nav_yeah <67997492+navya252001@users.noreply.github.com>
2020-09-11 09:57:24 +0530

    Updates

---
+++ precache-manifest.f35947d502e289ea290664cfbd1024a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8884e91835c1b84411b8eb409a05db1b",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "1ab199b7d74ac1796505",
+    "url": "/recipes-app/static/css/main.db441de9.chunk.css"
+  },
+  {
+    "revision": "2f8d44fb800ffc95ed0b",
+    "url": "/recipes-app/static/js/2.a52faf70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-app/static/js/2.a52faf70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab199b7d74ac1796505",
+    "url": "/recipes-app/static/js/main.d4400f84.chunk.js"
+  },
+  {
+    "revision": "e2cb1210ebd825da203b",
+    "url": "/recipes-app/static/js/runtime-main.60d9e62e.js"
+  }
+]);
